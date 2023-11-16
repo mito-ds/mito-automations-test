ba6d2cda0248e0292eef85e00b327005f0b4e6e5
Nate Rush <narush@wharton.upenn.edu>
2023-11-16 14:09:12 -0800

    Commit Message: add a new file

---
+++ automations/123123/automation.py
@@ -0,0 +1,13 @@
+from mitosheet.public.v3 import *
+import pandas as pd
+
+def 123123(file_name_import_csv_0):
+    df_export = pd.read_csv(file_name_import_csv_0)
+    
+    return df_export
+
+
+
+# Run the automation
+file_name_import_csv_0 = r"automations/123123/data/df_export.csv"
+123123(file_name_import_csv_0)
